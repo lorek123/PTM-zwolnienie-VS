28623e9f874d24590af3eccc52a9d1130a3b2860
lorek123 <lorek123@gmail.com>
2014-01-15 13:37:26 +0100

    elo

--- hid_con/hid_con.cpp
+++ hid_con/hid_con.cpp
@@ -7,27 +7,22 @@
 using namespace std;
 
 char buffer_out[64];
-char buffer_in[64];
-
+char haselko[18];
 int _tmain(int argc, _TCHAR* argv[])
 {
-	for (int i=0;i<64;i++) {
+	for (int i=0;i<18;i++) {
 		buffer_out[i]=0;	
-		buffer_in[i]=0;
 	}
 	buffer_out[1]=1;// pomaranczowa dioda
 	//buffer_out[1]=2;// zielona dioda
 	//buffer_out[1]=4;// czerwona dioda
 	//buffer_out[1]=8;// niebieska dioda
-
+	printf("Aby zagrac melodyjke podaj haslo\n");
+	scanf("%s", haselko);
+	printf("Wysy³amy: %s", haselko);
 	FindTheHID();
+	WriteOutputReport(haselko);
 	WriteOutputReport(buffer_out);
-	while (1){
-		ReadInputReport(buffer_in);
-		DisplayInputReport(buffer_in);
-		Sleep(1);
-	}
-	system("pause");
 
 	return 0;
 }
